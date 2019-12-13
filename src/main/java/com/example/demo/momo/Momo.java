c56afa21c0a63ad7f2b14fa9050991ee4930eefe
pleasemissyang <863641231@qq.com>
2019-12-13 10:44:20 +0800

    commit test

---
+++ src/main/java/com/example/demo/momo/Momo.java
@@ -0,0 +1,13 @@
+package com.example.demo.momo;
+
+public class Momo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
